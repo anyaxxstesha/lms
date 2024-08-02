343c2f990bcaec032315da98f15f6372ae05cad9
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-02 16:52:42 +0300

    register payment model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payment
 
 
 @admin.register(User)
@@ -8,3 +8,10 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'city', 'phone', 'is_staff', 'is_superuser')
     list_filter = ('is_staff', 'is_superuser')
     search_fields = ('email',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'course_paid', 'lesson_paid', 'payment_amount', 'date', 'payment_method')
+    list_filter = ('date', 'user')
+    search_fields = ('user',)
