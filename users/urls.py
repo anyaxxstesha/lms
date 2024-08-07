b19e766eb4eaae5bd1f2a1e78fb55a2256f2c47b
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-07 12:01:57 +0300

    add registration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView
+from users.views import PaymentListAPIView, UserCreateAPIView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,6 +12,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payment_list'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 
-from users.models import Payment
-from users.serializers import PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class PaymentListAPIView(ListAPIView):
@@ -12,3 +12,13 @@ class PaymentListAPIView(ListAPIView):
     filter_backends = (filters.OrderingFilter, DjangoFilterBackend)
     filterset_fields = ('payment_method', 'course_paid', 'lesson_paid')
     ordering_fields = ('date',)
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
