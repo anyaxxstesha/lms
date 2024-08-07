5827301f2830119e218f4d52772c940ca7b1651f
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-07 12:19:35 +0300

    add CRUD for user model

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
@@ -16,9 +16,27 @@ class PaymentListAPIView(ListAPIView):
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
-    queryset = User.objects.all()
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.objects.all()
