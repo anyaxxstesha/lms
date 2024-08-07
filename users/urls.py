7ef1e8760b6bc83bcab5340f577f104598220f8b
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-07 12:58:40 +0300

    fix bugs

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, UserCreateAPIView
+from users.views import PaymentListAPIView, UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, \
+    UserDestroyAPIView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,7 +13,11 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payment_list'),
-    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', UserListAPIView.as_view(), name='user_list'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_retrieve'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
 ]
