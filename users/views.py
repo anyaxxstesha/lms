d60720cafdff61a0a8a42b1fc63d1a1acaf3d4b9
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-19 16:39:24 +0300

    configure endpoint for payment creation

--- users/views.py
+++ users/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from materials.models import Course
 from users.models import Payment, User, Subscription
 from users.serializers import PaymentSerializer, UserSerializer
+from users.services import create_stripe_price, create_stripe_session, get_or_create_stripe_product
 
 
 class PaymentListAPIView(ListAPIView):
@@ -18,6 +19,29 @@ class PaymentListAPIView(ListAPIView):
     filter_backends = (filters.OrderingFilter, DjangoFilterBackend)
     filterset_fields = ('payment_method', 'course_paid', 'lesson_paid')
     ordering_fields = ('date',)
+
+
+class PaymentCreateAPIView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PaymentSerializer
+
+    def perform_create(self, serializer):
+        payment = serializer.save()
+        amount = payment.payment_amount
+
+        if payment.lesson_paid is not None:
+            obj = payment.lesson_paid
+        elif payment.course_paid is not None:
+            obj = payment.course_paid
+        else:
+            return Response(data={'detail': 'lesson or course must be provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+        product = get_or_create_stripe_product(obj)
+        price = create_stripe_price(amount, product)
+        session_id, payment_link = create_stripe_session(price)
+        payment.session_id = session_id
+        payment.link = payment_link
+        payment.save()
 
 
 class UserCreateAPIView(CreateAPIView):
